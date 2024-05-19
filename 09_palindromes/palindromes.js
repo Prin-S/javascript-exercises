55bf65090698338fde2f86241f92169f2508bfa9
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-05-19 16:52:49 +0700

    Completed exercises 9-11.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let comparison = '';
+    const regex = /\s|[.,!?]/g;
+    let processedText = text.replaceAll(regex, '').toLowerCase();
 
+    for (let i = processedText.length - 1; i >= 0; i--) {
+        comparison += processedText[i];
+    }
+
+    if (processedText != comparison) {
+        return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    } else if (num == 0) {
+        return 0;
+    }
 
+    fibSequence = [1, 1];
+
+    for (i = 2; i < num; i++) {
+        fibSequence.push(fibSequence[i - 2] + fibSequence[i - 1]);
+    }
+
+    return fibSequence.pop();
 };
 
 // Do not edit below this line
