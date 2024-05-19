b8502e2de2c5271375ea154d02f55f903b0f52b7
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-05-19 18:09:12 +0700

    Completed exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
 
+    return people.map(curr => {
+        if (curr.yearOfDeath == undefined) {
+            curr.yearOfDeath = new Date().getFullYear();
+        }
+        return curr;
+    }).reduce((oldest, curr) => {
+        if (curr.yearOfDeath - curr.yearOfBirth > oldestAge) {
+            oldestAge = curr.yearOfDeath - curr.yearOfBirth;
+            oldest = curr;
+        }
+        return oldest;
+    }, 0)
 };
 
 // Do not edit below this line
